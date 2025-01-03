75fbd2b6403b1c6fb0d46bff46fc17166b27e2d6
Wyrdix <wyrdix@gmail.com>
2025-01-03 22:06:31 +0100

    Add skeleton and tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<LightSwitch />
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-surface-900">
+	<h1>Welcome to SvelteKitA</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
